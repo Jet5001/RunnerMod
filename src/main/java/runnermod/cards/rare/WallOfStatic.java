203ff177d14c98deb5cc335cf9b82d64df088c22
Alex <alexander_baines@hotmail.co.uk>
2024-01-08 18:47:00 +0000

    Added wall of static

---
+++ src/main/java/runnermod/cards/rare/WallOfStatic.java
@@ -0,0 +1,48 @@
+package runnermod.cards.rare;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import runnermod.cards.BaseCard;
+import runnermod.character.RunnerCharacter;
+import runnermod.powers.InvestmentsPower;
+import runnermod.powers.WallOfStaticPower;
+import runnermod.util.CardStats;
+
+public class WallOfStatic extends BaseCard {
+    public static final String ID = makeID(WallOfStatic.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            RunnerCharacter.Enums.CARD_COLOR,
+            CardType.POWER,
+            CardRarity.RARE,
+            CardTarget.NONE,
+            2
+
+    );
+
+    private static final int MAGIC = 3;
+    private static final int MAGIC_UPG = 2;
+
+
+    public WallOfStatic()
+    {
+        super(ID,info);
+        setMagic(MAGIC,MAGIC_UPG);
+    }
+
+
+    @Override
+    public void upgrade()
+    {
+        if (!this.upgraded)
+        {
+            upgradeName();
+            this.isInnate = true;
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p,p,new WallOfStaticPower(p, magicNumber)));
+    }
+}
---
+++ src/main/java/runnermod/powers/WallOfStaticPower.java
@@ -0,0 +1,42 @@
+package runnermod.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.GainGoldAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+import static runnermod.RunnerMod.makeID;
+
+public class WallOfStaticPower extends BasePower implements CloneablePowerInterface {
+
+
+    public static final String POWER_ID = makeID("WallOfStaticPower");
+    public static final PowerType TYPE = PowerType.BUFF;
+    private static final boolean TURNBASED = true;
+    public WallOfStaticPower(AbstractCreature owner, int investmentAmount)
+    {
+        super(POWER_ID,TYPE,TURNBASED, owner, investmentAmount);
+    }
+
+    @Override
+    public void onApplyPower(AbstractPower power, AbstractCreature target, AbstractCreature source) {
+        super.onApplyPower(power, target, source);
+        if (!(target instanceof AbstractPlayer) && (power.type == PowerType.DEBUFF))
+        {
+            addToBot(new GainBlockAction(source,this.amount));
+        }
+
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new WallOfStaticPower(owner, amount);
+    }
+
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+}
